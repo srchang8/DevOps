6464edd7150f4f51399664ea1124c6b50fbe69c4
Stephen <srchang8@gmail.com>
2018-09-23 09:11:20 -0400

    add controllers and views

--- SpringMVC/src/main/java/org/stephen/controllers/IndexController.java
+++ SpringMVC/src/main/java/org/stephen/controllers/IndexController.java
@@ -1,9 +1,16 @@
 package org.stephen.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import org.stephen.models.User;
 
 @Controller
 public class IndexController {
@@ -13,4 +20,30 @@ public class IndexController {
         m.addAttribute("someAttribute", "someValue");
         return "index";
     }
+
+    //select home view to render by returning its name
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        System.out.println("Home Page Requested, locale = " + locale);
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        model.addAttribute("serverTime", formattedDate);
+
+        return "home";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String user(@Validated User user, Model model) {
+        System.out.println("User Page Requested");
+        model.addAttribute("userName", user.getUserName());
+        return "user";
+    }
+
+
+
 }
+
+
